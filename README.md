8c1ece9682b2fe1b72795cd44f5ee21e891d4b65
Malachi Vargas <mcvarga7@gmail.com>
2020-05-09 12:10:04 -0400

    Finished project

---
+++ README.md
@@ -0,0 +1,25 @@
+### Running the app
+Voting app created with react that implements managing state and passing data between components using props.
+
+Followed lesson in the "Full Stack React" book.
+
+<img src="images/screenshots/voting-app.png" width="90%"></img>
+
+
+1. Ensure you have `npm` installed.
+
+Follow the instructions for your platform [here](https://github.com/npm/npm).
+
+2. Install `http-server`
+
+````
+npm install
+````
+
+3. Boot the HTTP server
+
+````
+npm run server
+````
+
+The server is now running at [localhost:3000](localhost:3000)
---
+++ public/js/seed.js
@@ -0,0 +1,46 @@
+window.Seed = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
+
+  const products = [
+    {
+      id: 1,
+      title: 'Fibber',
+      description: 'Spread misinofrmation, meet like-minded fibbers, and get rewarded!',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/daniel.jpg',
+      productImageUrl: 'images/products/image-aqua.png',
+    },
+    {
+      id: 2,
+      title: 'Foes',
+      description: 'A fantasy league based on the stats of Twitch streamers.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/kristy.png',
+      productImageUrl: 'images/products/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Conomy',
+      description: 'Virtual games and services that are fueled by your earned financial interest.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/veronika.jpg',
+      productImageUrl: 'images/products/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Gin',
+      description: 'Free to play poker and card game site with a focus on esports.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/molly.png',
+      productImageUrl: 'images/products/image-yellow.png',
+    },
+  ];
+
+  return { products: products };
+}());
